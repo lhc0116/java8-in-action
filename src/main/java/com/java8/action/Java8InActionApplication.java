eefecaca44a44e799c14b36b697648b7f8afa3af
木子 <1018785442@163.com>
2019-09-08 17:23:51 +0800

    build

---
+++ src/main/java/com/java8/action/Java8InActionApplication.java
@@ -0,0 +1,13 @@
+package com.java8.action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8InActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8InActionApplication.class, args);
+	}
+
+}
