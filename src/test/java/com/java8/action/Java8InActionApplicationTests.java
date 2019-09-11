debb14c56b33ac202f8b3f98965db3825fa45720
木子 <1018785442@163.com>
2019-09-11 21:58:56 +0800

    commit

--- src/test/java/com/java8/action/Java8InActionApplicationTests.java
+++ src/test/java/com/java8/action/Java8InActionApplicationTests.java
@@ -2,13 +2,19 @@ package com.java8.action;
 
 import com.java8.action.entity.Apple;
 import com.java8.action.function.FunctionInterface1;
-import com.java8.action.function.Predicate;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 import static java.util.stream.Collectors.toList;
 
@@ -22,6 +28,32 @@ public class Java8InActionApplicationTests {
 		inventory = new ArrayList<>();
 	}
 
+	public <T, R> List<R> map(List<T> list, Function<T, R> f) {
+		List<R> result = new ArrayList<>();
+		list.forEach(i -> result.add(f.apply(i)));
+		return result;
+	}
+
+	@Test
+	public void test4() throws IOException {
+		List<Integer> lengths = map(Arrays.asList("黄蓉", "张三丰", "测试"), str -> str.length());
+		String result = processFile(br -> br.readLine() + br.readLine());
+	}
+
+	public String processFile(BufferedReaderProcessor brp) throws IOException {
+		try (BufferedReader bufferedReader = new BufferedReader(new FileReader("data.txt"))) {
+			return brp.process(bufferedReader);
+		}
+	}
+
+	@FunctionalInterface
+	public interface BufferedReaderProcessor {
+		String process(BufferedReader br) throws IOException;
+	}
+
+	public Callable<String> fetch() {
+		return () -> "测试Lambda表达式";
+	}
 
 	/**
 	 * 自定义函数式接口
