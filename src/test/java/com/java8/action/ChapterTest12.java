bae614247261e4009c3f3b34299d40a0523469ed
lhc0116 <1018785442@163.com>
2019-10-09 07:56:53 +0800

    commit

--- src/test/java/com/java8/action/ChapterTest12.java
+++ src/test/java/com/java8/action/ChapterTest12.java
@@ -2,12 +2,12 @@ package com.java8.action;
 
 import org.junit.Test;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.ZoneId;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author li huaichuan
@@ -16,8 +16,32 @@ import java.time.temporal.ChronoField;
 public class ChapterTest12 {
 
     @Test
-    public void test2() {
+    public void test3() {
+        Duration d1 = Duration.between(LocalDateTime.of(2019, 10, 7, 15, 55, 55, 888), LocalDateTime.now());
+        Duration d2 = Duration.between(LocalTime.of(17, 55, 10), LocalTime.now());
+        Duration d3 = Duration.between(Instant.ofEpochMilli(1570544602000L), Instant.now());
+        System.out.println(d3.toHours());
+        //Duration对象用秒和纳秒来衡量时间的长短，所以入参不能使用LocalDate类型, 否则抛UnsupportedTemporalTypeException: Unsupported unit: Seconds
+        //Duration.between(LocalDate.of(2019, 10, 7), LocalDate.now());
 
+        //如果想要对多个时间对象进行日期运算，可以用Period
+        Period p1 = Period.between(LocalDate.of(2018, 8, 30), LocalDate.now());
+        System.out.println(p1.getYears() + "\t" + p1.getMonths() + "\t" + p1.getDays());
+        //工厂方法介绍
+        Duration threeMinutes = Duration.ofMinutes(3);
+        threeMinutes = Duration.of(3, ChronoUnit.MINUTES);
+        Period tenDays = Period.ofDays(10);
+        Period threeWeeks = Period.ofWeeks(3);
+        Period twoYearsSixMonthsOneDay = Period.of(2, 6, 1);
+    }
+
+    /**
+     * 计算机角度的日期和时间格式 instant
+     */
+    @Test
+    public void test2() {
+        assertTrue(Instant.ofEpochSecond(3).getEpochSecond() == Instant.ofEpochSecond(2, 1_000_000_000).getEpochSecond());
+        assertTrue(Instant.ofEpochSecond(4, -1_000_000_000).getEpochSecond() == Instant.ofEpochSecond(2, 1_000_000_000).getEpochSecond());
     }
 
     /**
