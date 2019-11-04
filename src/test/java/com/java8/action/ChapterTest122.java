7b1272a1dbda5aad3a1624cc05e2c0a2e9a4e74d
lhc0116 <1018785442@163.com>
2019-11-04 23:25:12 +0800

    commit

--- src/test/java/com/java8/action/ChapterTest122.java
+++ src/test/java/com/java8/action/ChapterTest122.java
@@ -13,9 +13,75 @@ import java.util.concurrent.CompletableFuture;
 public class ChapterTest122 {
 
 	@Test
+	public void test10() {
+		CompletableFuture<String> f1 = CompletableFuture.supplyAsync(this::get);
+		CompletableFuture<Void> f2 = CompletableFuture.allOf();
+		CompletableFuture<Void> f3 = f1.runAfterEither(f2, () -> System.out.println("执行一个任务，没有入参"));
+		f3.join();
+	}
+
+	public String get() {
+		delay();//这里会延时一秒钟
+		return "CompletableFuture 1";
+	}
+
+	@Test
+	public void test9() {
+		CompletableFuture<String> f1 = CompletableFuture.supplyAsync(this::get);
+		CompletableFuture<String> f2 = CompletableFuture.completedFuture("CompletableFuture 2");
+		CompletableFuture<Integer> f3 = f1.applyToEither(f2, res -> {
+			System.out.println("res = " + res);
+			return res.length();
+		});
+		System.out.println("f3.join() = " + f3.join());
+	}
+
+	public void delay() {
+		try {
+			Thread.sleep(1000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void test8() {
+		CompletableFuture<String> f1 = CompletableFuture.completedFuture("CompletableFuture 1");
+		CompletableFuture<String> f2 = CompletableFuture.supplyAsync(this::get);
+		CompletableFuture<Void> both = f1.acceptEither(f2, System.out::println);
+		both.join();
+	}
+
+	@Test
+	public void test7() {
+		CompletableFuture<Integer> f1 = CompletableFuture.completedFuture(9523);
+		CompletableFuture<String> f2 = CompletableFuture.supplyAsync(() -> "");
+		CompletableFuture<Void> both = f1.runAfterBoth(f2, () -> System.out.println("执行一个任务，没有入参"));
+		both.join();
+	}
+
+	@Test
+	public void test6() {
+		CompletableFuture<Integer> f1 = CompletableFuture.completedFuture(9523);
+		CompletableFuture<String> f2 = CompletableFuture.supplyAsync(this::get);
+		CompletableFuture<Void> both = f1.thenAcceptBoth(f2, (num, str) -> System.out.println("num = " + num + ", str = " + str));
+		both.join();
+	}
+
+	@Test
+	public void test5() {
+		CompletableFuture<String> f = CompletableFuture.supplyAsync(() -> {
+			//测试抛异常后,exceptionally()方法接受并处理
+			//int x = 1 / 0;
+			return "这是一个栗子";
+		}).exceptionally(ex -> ex.getCause().getMessage());
+		System.out.println("f.join() = " + f.join());
+	}
+
+	@Test
 	public void test4() {
 		CompletableFuture<String> f = CompletableFuture.supplyAsync(() -> {
-			//测试抛异常后,handleAsync()方法接受并处理
+			//测试抛异常后,whenComplete()方法接受并处理
 			int x = 1 / 0;
 			return "这是一个栗子";
 		}).whenComplete((res, ex) -> {
@@ -46,7 +112,7 @@ public class ChapterTest122 {
 	@Test
 	public void test2() {
 		CompletableFuture<Void> f = CompletableFuture.supplyAsync(() -> {
-			//测试抛异常后,handleAsync()方法接受并处理
+			//测试抛异常后,handle()方法接受并处理
 			int x = 1 / 0;
 			return "这是一个栗子";
 		}).handle((res, ex) -> {
@@ -63,16 +129,4 @@ public class ChapterTest122 {
 		f.join();
 	}
 
-	public String get() {
-		delay();
-		return "异步任务结果";
-	}
-
-	public void delay() {
-		try {
-			Thread.sleep(1000L);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
 }
