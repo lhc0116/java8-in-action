34354666a1816cd9b827d9be455b02f2c5a5504a
lhc0116 <1018785442@163.com>
2019-11-05 22:58:30 +0800

    commit

--- src/main/java/com/java8/action/controller/AsyncController.java
+++ src/main/java/com/java8/action/controller/AsyncController.java
@@ -15,29 +15,30 @@ import java.util.concurrent.CompletableFuture;
 @RestController
 public class AsyncController {
 
-	@GetMapping("/forward")
-	public CompletableFuture<ModelAndView> forward() {
-		return CompletableFuture.supplyAsync(() -> {
-			this.delay();
-			RedirectView redirectView = new RedirectView("https://www.cnblogs.com/qingshanli/");
-			redirectView.addStaticAttribute("hint", "CompletableFuture组装ModelAndView视图,异步返回结果");
-			return new ModelAndView(redirectView);
-		});
-	}
+    @GetMapping("/redirect")
+    public CompletableFuture<ModelAndView> redirect() {
+        return CompletableFuture.supplyAsync(() -> {
+            this.delay();
+            RedirectView redirectView = new RedirectView("https://www.cnblogs.com/qingshanli/");
+            redirectView.addStaticAttribute("hint", "CompletableFuture组装ModelAndView视图,异步返回结果");
+            return new ModelAndView(redirectView);
+        });
+    }
 
-	@GetMapping("/async")
-	public CompletableFuture<String> async() {
-		return CompletableFuture.supplyAsync(() -> {
-			this.delay();
-			return "CompletableFuture作为Controller的返回值,异步返回结果";
-		});
-	}
+    @GetMapping("/async")
+    public CompletableFuture<String> async() {
+        System.out.println("async method start");
+        return CompletableFuture.supplyAsync(() -> {
+            this.delay();
+            return "CompletableFuture作为Controller的返回值,异步返回结果";
+        }).whenComplete((res, ex) -> System.out.println("async method completely, res = " + res + ", ex = " + ex));
+    }
 
-	public void delay() {
-		try {
-			Thread.sleep(3000L);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+    public void delay() {
+        try {
+            Thread.sleep(3000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
